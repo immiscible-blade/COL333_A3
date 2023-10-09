fb2108e782fa469c6980b4a638071ad3a45f2eab
immiscible-blade <om.dehlantube@gmail.com>
2023-10-09 14:15:51 +0530

    Structure Made

---
+++ include/inputmaker.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class inputmaker
+{
+private:
+    std::vector<std::vector<int>> graph;
+    int graphsize;
+    int k1;
+    int k2;
+public:
+    inputmaker(std::string filename);
+    void initialize_cnf(std::string outfile); // minisat initial line
+    void make_subgraphs_clauses(std::string outfile); // Ais and Bis
+    void make_edges_clauses(std::string outfile); // adjacency matrix flattened
+    void make_fullsubgraph_clauses(std::string outfile); // connected subgraph
+    void make_atmostk_clauses(std::string outfile, int k, int start_index); // one function called twice for both sub graph, use start_index to differentiate
+    ~inputmaker();
+};
+
---
+++ src/inputmaker.cpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "../include/inputmaker.hpp"
+
+inputmaker::inputmaker(std::string filename)
+{
+}
+
+inputmaker::~inputmaker()
+{
+}
+
+void inputmaker::make_atmostk_clauses(std::string outfile, int k, int start_index)
+{
+}
+
+void initialize_cnf(std::string outfile)
+{
+}
+
+void make_subgraphs_clauses(std::string outfile)
+{
+}
+
+void make_edges_clauses(std::string outfile)
+{
+}
+
+void make_fullsubgraph_clauses(std::string outfile)
+{    
+}
