4cfa4211599be2e18ad7be376802ad050d77f158
immiscible-blade <om.dehlantube@gmail.com>
2023-10-09 18:21:29 +0530

    great progress

--- src/inputmaker.cpp
+++ src/inputmaker.cpp
@@ -6,6 +6,8 @@ inputmaker::inputmaker(std::string filename, std::string outfile):outfile(outfil
     myfile.open(filename);
     int m;
     myfile >> graphsize >> m >> k1 >> k2;
+    k1 = graphsize-k1;
+    k2 = graphsize-k2;
     graph = (bool*)malloc((graphsize*graphsize) * sizeof(bool));
     for (int i = 0; i < graphsize; i++)
     {
@@ -32,7 +34,7 @@ inputmaker::inputmaker(std::string filename, std::string outfile):outfile(outfil
 void inputmaker::initialize_cnf()
 {
     aout << "c Hi There its a comment\n";
-    aout << "p cnf " << (2*graphsize + (graphsize)*(graphsize-1)/2 + (graphsize-1)*(graphsize-k1) + (graphsize-1)*(graphsize - k2)) << " " << "(no. of clauses)\n";
+    aout << "p cnf " << (2*graphsize + ((graphsize)*(graphsize-1))/2 + (graphsize-1)*(graphsize-k1) + (graphsize-1)*(graphsize - k2)) << " " << "(no. of clauses)\n";
 }
 
 void inputmaker::make_subgraphs_clauses()
@@ -69,21 +71,59 @@ void inputmaker::make_fullsubgraph_clauses()
     {
         for (int j = 1; j < i; j++)
         {
-            aout << i << " " << j << " " << 2*graphsize + j + (i-1)*(i-2)/2 << " 0\n";
+            aout << i << " " << j << " " << 2*graphsize + j + ((i-1)*(i-2))/2 << " 0\n";
         }
     }
     for (int i = 1; i < graphsize+1; i++)
     {
         for (int j = 1; j < i; j++)
         {
-            aout << i+graphsize << " " << j+graphsize << " " << 2*graphsize + j + (i-1)*(i-2)/2 << " 0\n";
+            aout << i+graphsize << " " << j+graphsize << " " << 2*graphsize + j + ((i-1)*(i-2))/2 << " 0\n";
         }
     }
 }
 
 void inputmaker::make_atmostk_clauses(int k, bool forwho)
 {
+    int start_index;
+    int startAB;
+    if (forwho)
+    {
+        start_index = 2*graphsize + ((graphsize-1)*(graphsize))/2 + 1;
+        startAB = 1;
+    }
+    else
+    {
+        start_index = 2*graphsize + ((graphsize-1)*(graphsize))/2 + (graphsize-1)*(graphsize-k1) + 1;
+        startAB = graphsize+1;
+    }
+    int temp_index = start_index;
+    int temp_sAB = startAB;
+    for (int i = 0; i < graphsize-1; i++)
+    {
+        aout << -temp_sAB << " " << temp_index << " 0\n";
+        temp_sAB++;
+        temp_index += (graphsize - k);
+    }
+    temp_index = start_index+1;
+    temp_sAB = startAB;
+    for (int i = 1; i < k; i++)
+    {
+        aout << -temp_index << " 0\n";
+        temp_index++;
+    }
 
+    temp_index = start_index;
+    for (int i = 2; i < graphsize; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            aout << -temp_index << " " << temp_index+k << " 0\n";
+            temp_index++;
+        }
+    }
+
+    //last 2 remaining
 }
 
 void inputmaker::close_out()
