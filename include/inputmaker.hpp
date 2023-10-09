07840071ccbc96e5ce703a098fe40f39be96fbe4
immiscible-blade <om.dehlantube@gmail.com>
2023-10-09 16:37:19 +0530

    made graph

--- include/inputmaker.hpp
+++ include/inputmaker.hpp
@@ -9,17 +9,17 @@
 class inputmaker
 {
 private:
-    std::vector<std::vector<int>> graph;
+    bool* graph; // use as graph[i*graphsize + j]
     int graphsize;
     int k1;
     int k2;
 public:
-    inputmaker(std::string filename);
+    inputmaker(std::string filename); // make graph
     void initialize_cnf(std::string outfile); // minisat initial line
     void make_subgraphs_clauses(std::string outfile); // Ais and Bis
     void make_edges_clauses(std::string outfile); // adjacency matrix flattened
     void make_fullsubgraph_clauses(std::string outfile); // connected subgraph
     void make_atmostk_clauses(std::string outfile, int k, int start_index); // one function called twice for both sub graph, use start_index to differentiate
-    ~inputmaker();
+    // ~inputmaker();
 };
 
--- src/inputmaker.cpp
+++ src/inputmaker.cpp
@@ -1,15 +1,32 @@
-#pragma once
-
 #include "../include/inputmaker.hpp"
 
 inputmaker::inputmaker(std::string filename)
 {
+    std::ifstream myfile;
+    myfile.open(filename);
+    int m;
+    myfile >> graphsize >> m >> k1 >> k2;
+    graph = (bool*)malloc((graphsize*graphsize) * sizeof(bool));
+    for (int i = 0; i < graphsize; i++)
+    {
+        for (int j = 0; j < graphsize; j++)
+        {
+            graph[i*graphsize + j] = false;
+        }
+    }
+    int e1, e2;
+    for (int i = 0; i < m; i++)
+    {
+        myfile >> e1 >> e2;
+        graph[(e1-1)*graphsize + (e2-1)] = true;
+        graph[(e2-1)*graphsize + (e1-1)] = true;
+    }
 }
 
-inputmaker::~inputmaker()
-{
-}
-
+// inputmaker::~inputmaker()
+// {
+// }
+// myfile.open("example.txt", ios::app)
 void inputmaker::make_atmostk_clauses(std::string outfile, int k, int start_index)
 {
 }
